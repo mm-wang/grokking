81cb9a230d341c5ca6805f6c9e24794d5d32c0d6
margaret <mwang27@gmail.com>
2023-03-15 17:41:52 +0000

    Adding stacks + card view

---
+++ iOS/Scrumdinger/Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,24 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Margaret on 3/15/23.
+//
+
+import Foundation
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design", attendees: ["Margaret", "Will", "Natasha", "Sabrina"], lengthInMinutes: 10, theme: .periwinkle),
+        DailyScrum(title: "Dev", attendees: ["Margaret", "Luis", "Darla", "Chad", "Jean", "Jeremy", "Timothy"], lengthInMinutes: 15, theme: .sky),
+        DailyScrum(title: "Strategy", attendees: ["Emma", "Eleanor", "Rafe"], lengthInMinutes: 5, theme: .navy)
+    ]
+}
