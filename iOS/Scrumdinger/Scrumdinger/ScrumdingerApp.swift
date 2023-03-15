60d7cef49a5b098abfa14a47b866b7863b358b3e
margaret <mwang27@gmail.com>
2023-03-15 16:36:29 +0000

    Learning iOS with Scrumdinger

---
+++ iOS/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Margaret on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
